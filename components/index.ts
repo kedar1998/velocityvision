b6a91e0de9fde86035c68848dfaa5a0c975a93a2
kedar1998 <interns@zoth.io>
2023-09-11 18:45:28 +0530

    Added search bar with headless UI

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,16 @@
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+
 
 export {
     Hero,
     Navbar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer
 }
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
     btnType?: "button" | "submit"
     handleClick?: MouseEventHandler<HTMLButtonElement>
 }
+
+export interface searchManufacturerProps {
+    manufacturer: string
+    setManufacturer: (manufacturer: string) => void
+}
